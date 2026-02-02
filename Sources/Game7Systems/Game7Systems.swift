f47c36b57765891257cba58b89b959a0191a0ac4
cch <sjosh4374@icloud.com>
2026-02-02 16:24:29 +0800

    some benchmark

--- Sources/Game7Systems/Game7Systems.swift
+++ Sources/Game7Systems/Game7Systems.swift
@@ -9,8 +9,8 @@ struct Game7Systems {
     public static func main() async throws {
         // ##################################################
         // parameter
-            let ITER_NUM = 4
-            let totalEntityNum = 4096 * 512
+            let ITER_NUM = 16
+            let totalEntityNum = 4096 * 256
             let seed = UInt32(12345)
             let emplaceStrategy = GameSettings.emplaceStrategyProb.prob_075
             let printWorldFlag = false
