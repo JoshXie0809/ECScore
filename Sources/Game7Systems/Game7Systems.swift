19bfb89a18e37a2e2e2e6afe9c7b3a7d17b70781
Josh <sjosh4374@gamil.com>
2026-02-03 17:53:52 +0800

    new bench

--- Sources/Game7Systems/Game7Systems.swift
+++ Sources/Game7Systems/Game7Systems.swift
@@ -8,10 +8,10 @@ struct Game7Systems {
     public static func main() async throws {
         // ##################################################
         // parameter
-            let ITER_NUM = 1
+            let ITER_NUM = 16
             let totalEntityNum = 4096 * 512
             let seed = UInt32(12345)
-            let emplaceStrategy = GameSettings.emplaceStrategyProb.prob_100
+            let emplaceStrategy = GameSettings.emplaceStrategyProb.prob_050
             let printWorldFlag = false
         // ##################################################
 
