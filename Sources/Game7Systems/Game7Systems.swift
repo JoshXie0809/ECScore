fcd678484b5993f1b1b717232b70ccb96c6ed2af
cch <sjosh4374@icloud.com>
2026-02-05 22:39:33 +0800

    add more random ( seed = iter + 12345)

--- Sources/Game7Systems/Game7Systems.swift
+++ Sources/Game7Systems/Game7Systems.swift
@@ -21,7 +21,7 @@ struct Game7Systems {
             let gs = GameSettings(
                 iterId: iter,
                 ttEn: totalEntityNum, 
-                seed: seed, 
+                seed: seed + UInt32(iter), 
                 printWorldFlag: printWorldFlag,
                 emplaceStrategy: emplaceStrategy
             )
@@ -75,17 +75,6 @@ let ta1 = clock.now
 let tb0 = clock.now
 // ---------------------------------------------------------
 
-// bench-mark-system-order
-// m_systems.emplace_back(createMovementSystem(m_entities));
-// m_systems.emplace_back(createDataSystem(m_entities));
-// if (m_addMoreComplexSystem == add_more_complex_system_t::UseMoreComplexSystems) {
-//   m_systems.emplace_back(createMoreComplexSystem(m_entities));
-//   m_systems.emplace_back(createHealthSystem(m_entities));
-//   m_systems.emplace_back(createDamageSystem(m_entities));
-//   m_systems.emplace_back(createSpriteSystem(m_entities));
-//   m_systems.emplace_back(createRenderSystem(m_entities));
-// }
-
     // system update
     let fakeDt = Duration.nanoseconds(1_000_000_000 / 120)
     world.tick(fakeDt)
@@ -116,89 +105,6 @@ let tb1 = clock.now
     )
 }
 
-// @inline(__always) 
-// func createEntities(
-//     _ world: borrowing World, _ gs: GameSettings, 
-//     _ rng: inout Xoshiro128, _ empToken: EMP_TOKEN
-// ) -> (Int, Int, Int) 
-// {
-//     let totalEntityNum = gs.ttEn
-//     var (heroCount, monsterCount, npcCount) = (0, 0, 0)
-//     let prob = gs.emplaceStrategy.rawValue
-    
-//     emplace(world.base, tokens: empToken) {
-//         entities, pack in
-//         var ( plSt, hSt, dmgSt, posSt, 
-//             dataSt, spSt, dirSt, emptySt ) = pack.storages
-//         for i in 0..<totalEntityNum {
-//             var targetType: PlayerType? = nil
-
-//             if i == 0 {
-//                 targetType = .hero
-//             } else if (i % 6) == 0 {
-//                 let roll = rng.next() % 100
-//                 targetType = (roll < 3) ? .npc : (roll < 30) ? .hero : .monster
-//             } else if (i % 4) == 0 {
-//                 targetType = .hero
-//             } else if (i % 2) == 0 {
-//                 targetType = .monster
-//             }
-
-//             if let type = targetType {
-//                 switch type {
-//                 case .hero: heroCount += 1
-//                 case .monster: monsterCount += 1
-//                 case .npc: npcCount += 1
-//                 }
-
-//                 let (p, h, d) = World.Spawner.spawnEntityComponent(&rng, type)
-
-//                 let entity = entities.createEntity()
-//                 plSt.addComponent(entity, p)
-//                 hSt.addComponent(entity, h)
-//                 dmgSt.addComponent(entity, d)
-//                 spSt.addComponent(entity, SpriteComponent()) 
-
-//                 let roll1 = rng.next() 
-//                 let roll2 = rng.next()
-//                 let roll3 = rng.next() 
-//                 let roll4 = rng.next() 
-//                 let roll5 = rng.next()
-
-//                 if roll1 % 100 > prob { // empty
-//                     emptySt.addComponent(entity, EmptyComponent()) 
-//                     continue
-//                 } 
-
-//                 if (roll2 % 100 & 1) == 0 { // all-component
-//                     dataSt.addComponent(entity, DataComponent(seed: roll3))
-//                 } 
-
-//                 // minimal
-//                 let pos = PositionComponent(
-//                     x: Float(roll4 % World.maxX), 
-//                     y: Float(roll5 % World.maxY)
-//                 )
-
-//                 posSt.addComponent(entity, pos)
-//                 dirSt.addComponent(entity, DirectionComponent())    
-
-//             }
-//         }
-//     }
-//     return (heroCount, monsterCount, npcCount) 
-// }
-
-typealias EMP_TOKEN = (
-    TypeToken<PlayerComponent>, 
-    TypeToken<HealthComponent>, 
-    TypeToken<DamageComponent>, 
-    TypeToken<PositionComponent>, 
-    TypeToken<DataComponent>, 
-    TypeToken<SpriteComponent>, 
-    TypeToken<DirectionComponent>,
-    TypeToken<EmptyComponent>
-)
 
 // same as bench-mark-logic
 // https://github.com/abeimler/ecs_benchmark/blob/master/benchmark/benchmarks/BaseECSBenchmark.h#L185
@@ -288,3 +194,109 @@ func createEntities2(
     }
     return (heroCount, monsterCount, npcCount) 
 }
+
+
+
+
+
+
+
+
+
+// bench-mark-system-order
+// m_systems.emplace_back(createMovementSystem(m_entities));
+// m_systems.emplace_back(createDataSystem(m_entities));
+// if (m_addMoreComplexSystem == add_more_complex_system_t::UseMoreComplexSystems) {
+//   m_systems.emplace_back(createMoreComplexSystem(m_entities));
+//   m_systems.emplace_back(createHealthSystem(m_entities));
+//   m_systems.emplace_back(createDamageSystem(m_entities));
+//   m_systems.emplace_back(createSpriteSystem(m_entities));
+//   m_systems.emplace_back(createRenderSystem(m_entities));
+// }
+
+
+
+// @inline(__always) 
+// func createEntities(
+//     _ world: borrowing World, _ gs: GameSettings, 
+//     _ rng: inout Xoshiro128, _ empToken: EMP_TOKEN
+// ) -> (Int, Int, Int) 
+// {
+//     let totalEntityNum = gs.ttEn
+//     var (heroCount, monsterCount, npcCount) = (0, 0, 0)
+//     let prob = gs.emplaceStrategy.rawValue
+    
+//     emplace(world.base, tokens: empToken) {
+//         entities, pack in
+//         var ( plSt, hSt, dmgSt, posSt, 
+//             dataSt, spSt, dirSt, emptySt ) = pack.storages
+//         for i in 0..<totalEntityNum {
+//             var targetType: PlayerType? = nil
+
+//             if i == 0 {
+//                 targetType = .hero
+//             } else if (i % 6) == 0 {
+//                 let roll = rng.next() % 100
+//                 targetType = (roll < 3) ? .npc : (roll < 30) ? .hero : .monster
+//             } else if (i % 4) == 0 {
+//                 targetType = .hero
+//             } else if (i % 2) == 0 {
+//                 targetType = .monster
+//             }
+
+//             if let type = targetType {
+//                 switch type {
+//                 case .hero: heroCount += 1
+//                 case .monster: monsterCount += 1
+//                 case .npc: npcCount += 1
+//                 }
+
+//                 let (p, h, d) = World.Spawner.spawnEntityComponent(&rng, type)
+
+//                 let entity = entities.createEntity()
+//                 plSt.addComponent(entity, p)
+//                 hSt.addComponent(entity, h)
+//                 dmgSt.addComponent(entity, d)
+//                 spSt.addComponent(entity, SpriteComponent()) 
+
+//                 let roll1 = rng.next() 
+//                 let roll2 = rng.next()
+//                 let roll3 = rng.next() 
+//                 let roll4 = rng.next() 
+//                 let roll5 = rng.next()
+
+//                 if roll1 % 100 > prob { // empty
+//                     emptySt.addComponent(entity, EmptyComponent()) 
+//                     continue
+//                 } 
+
+//                 if (roll2 % 100 & 1) == 0 { // all-component
+//                     dataSt.addComponent(entity, DataComponent(seed: roll3))
+//                 } 
+
+//                 // minimal
+//                 let pos = PositionComponent(
+//                     x: Float(roll4 % World.maxX), 
+//                     y: Float(roll5 % World.maxY)
+//                 )
+
+//                 posSt.addComponent(entity, pos)
+//                 dirSt.addComponent(entity, DirectionComponent())    
+
+//             }
+//         }
+//     }
+//     return (heroCount, monsterCount, npcCount) 
+// }
+
+typealias EMP_TOKEN = (
+    TypeToken<PlayerComponent>, 
+    TypeToken<HealthComponent>, 
+    TypeToken<DamageComponent>, 
+    TypeToken<PositionComponent>, 
+    TypeToken<DataComponent>, 
+    TypeToken<SpriteComponent>, 
+    TypeToken<DirectionComponent>,
+    TypeToken<EmptyComponent>
+)
+
