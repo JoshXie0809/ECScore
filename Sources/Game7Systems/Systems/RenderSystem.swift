59a02262cf782a17cfa9030153434d3c768ee87d
cch <sjosh4374@icloud.com>
2026-02-07 23:30:01 +0800

    test with tag intersection

--- Sources/Game7Systems/Systems/RenderSystem.swift
+++ Sources/Game7Systems/Systems/RenderSystem.swift
@@ -3,10 +3,12 @@ import ECScore
 struct RenderSystem {
     // 定義所需組件的 Token
     let renderToken: (TypeToken<PositionComponent>, TypeToken<SpriteComponent>)
+    // let withTagToken: TypeToken<EmptyComponent>
 
     init(base: borrowing VBPF) {
         // 預先取得組件位置，確保效能
         self.renderToken = interop(base, PositionComponent.self, SpriteComponent.self)
+        self.withTagToken = interop(base, EmptyComponent.self)
     }
 
     @inline(__always)
@@ -16,6 +18,7 @@ struct RenderSystem {
         let width = world.frameBuffer.width
 
         let logic = RenderLogic(buffer: buffer, height: height, width: width)
+        // view(base: world.base, with: renderToken, withTag: withTagToken, logic)
         view(base: world.base, with: renderToken, logic)
 
         _fixLifetime(buffer)
