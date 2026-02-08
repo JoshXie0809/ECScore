e8dc6ec419f645de3bbbada7dde4c157d7b9e0eb
cch <sjosh4374@icloud.com>
2026-02-09 00:21:27 +0800

    padding 4

--- Sources/ECScore/SparseSetDataContainer/Page64.swift
+++ Sources/ECScore/SparseSetDataContainer/Page64.swift
@@ -24,7 +24,7 @@ struct Page64: CustomStringConvertible {
     @inline(__always) private(set) var entityOnPage = 
         ContiguousArray<SparseSetEntry>(
             repeating: SparseSetEntry(compArrIdx: Int16(-1)), 
-            count: 65 // 64 + padding 1 = 65 
+            count: 68 // 64 + padding 4 = 68 (index: 64, 65, 66, 67) can record information
         )
 
     var description: String {
